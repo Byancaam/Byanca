50e6528cc6f33b680fc98bce67aabed60dbceea6
bamarantemagalha <bamarantemagalha@apple.com>
2023-10-24 05:04:59 -0300

    [challenge] - feat: creating the boilerplate for some components

---
+++ src/components/SearchSynonyms.tsx
@@ -0,0 +1,11 @@
+export default function SearchSynonyms() {
+  return (
+    <div>
+      <input
+        type="text"
+        name="word"
+        placeholder="Digite uma palavra para visualizar seus sinonimos"
+      />
+    </div>
+  );
+}
